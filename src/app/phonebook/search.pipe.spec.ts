7a366dda9c97354b940d0931fa058c6d793f6289
viktoriia-honcharova <vika.honcharova.619853@gmail.com>
2023-03-17 11:37:49 +0200

    Auto-generated commit [ci skip]

--- src/app/phonebook/search.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SearchnamePipe } from './search.pipe';
-
-describe('SearchnamePipe', () => {
-  it('create an instance', () => {
-    const pipe = new SearchnamePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
